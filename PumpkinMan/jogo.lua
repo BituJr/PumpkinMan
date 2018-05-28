c6a761806ab24f4216680248de6db021d9429381
Ivan Bitu <ivanbbj@gmail.com>
2018-05-28 09:39:42 -0300

    Update jogo.lua

--- PumpkinMan/jogo.lua
+++ PumpkinMan/jogo.lua
@@ -10,10 +10,8 @@ display.setStatusBar(display.HiddenStatusBar)
 function scene:create(event)
 
     local sceneGroup = self.view
-	
-	
 
-	-- Game Variables
+	-- variáveis do jogo
 	screenXwidth = display.contentWidth
 	screenXcenter = screenXwidth/2
 	screenYheight = display.contentHeight
@@ -22,52 +20,69 @@ function scene:create(event)
 	inEvent = 0
 	eventRun = 0
 
-	-- Groups
+	-- Grupos
 	blocks = display.newGroup()
 	characters = display.newGroup()
 	screen = display.newGroup()
+	obstaculos = display.newGroup()
+	heads = display.newGroup()
 	
 	require("scoreboard")
-	require("ground")
+	require("chao")
 	require("background")
 	require("collisions")
 	require("events")
-	require("hero")
+	require("personagem")
 	require("game_over")	
 	require("speed")
+	require("obstaculo")
+	require("head")
 	
 	screen:insert(backbackground)
-	screen:insert(backgroundfar1)
-	screen:insert(backgroundfar2)
-	screen:insert(backgroundnear1)
-	screen:insert(backgroundnear2)
+	screen:insert(backgroundM1)
+	screen:insert(backgroundM2)
+	screen:insert(backgroundA1)
+	screen:insert(backgroundA2)
 	screen:insert(blocks)
-	screen:insert(hero)
+	screen:insert(personagem)
+	screen:insert(obstaculos)
+	screen:insert(heads)
 	screen:insert(collisionRect)
 	screen:insert(scoreText)
 	screen:insert(gameOver)
 
 
 	local function update( event )
-		if hero.isAlive then
+		if personagem.isAlive then
 			checkCollisions()
 			updateSpeed()
 			updateBackgrounds()
 			updateBlocks()
 			updateCharacter()
+			updateObstaculos()
+			updateHeads()
 		else
 			endGame()
 		end
 	end
 
-	function touched( event )
-		if hero.isAlive then
-			if(event.phase == "began") then
-				if(onGround) then
-					hero.accel = hero.accel + 20
-					speed = baseSpeed
-				end
-			end
+function touched( event )
+		if(event.phase == "began") then
+        if(event.x < 241) then
+            if(onGround) then
+                personagem.accel = personagem.accel + 20
+            end
+        else
+            for a=1, heads.numChildren, 1 do
+                if(heads[a].isAlive == false) then
+                    heads[a].isAlive = true
+                    heads[a].x = personagem.x + 50
+                    heads[a].y = personagem.y
+                    break
+                end
+            end
+        end
+    
 		else
 			if(event.x < gameOver.x + 150 and event.x > gameOver.x - 150 and event.y < gameOver.y + 95 and event.y > gameOver.y - 95) then
 				restartGame()
