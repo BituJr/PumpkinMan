9ce712524ded1958941973a0abba741ff7c40f45
Ivan Bitu <ivanbbj@gmail.com>
2018-05-28 09:34:20 -0300

    Update collisions.lua

--- PumpkinMan/collisions.lua
+++ PumpkinMan/collisions.lua
@@ -1,17 +1,29 @@
+-- Criando Colisões
+
 function checkCollisions()
 	wasOnGround = onGround
 
 	for a = 1, blocks.numChildren, 1 do
 		if(collisionRect.y - 10> blocks[a].y - 170 and blocks[a].x - 40 < collisionRect.x and blocks[a].x + 40 > collisionRect.x) then
 			speed = 0
-			killHero()
+			killPersonagem()
+		end
+	end
+	
+	--stop the game if the monster runs into a obstaculo wall
+	for a = 1, obstaculos.numChildren, 1 do
+		if(obstaculos[a].isAlive == true) then
+			if(collisionRect.y - 10> obstaculos[a].y - 60 and obstaculos[a].x - 50 < collisionRect.x and obstaculos[a].x + 25 > collisionRect.x) then
+				killPersonagem()
+				speed = 0
+			end
 		end
 	end
 
 
 	for a = 1, blocks.numChildren, 1 do
-		if(hero.y >= blocks[a].y - 170 and blocks[a].x < hero.x + 60 and blocks[a].x > hero.x - 60) then
-			hero.y = blocks[a].y - 171
+		if(personagem.y >= blocks[a].y - 170 and blocks[a].x < personagem.x + 60 and blocks[a].x > personagem.x - 60) then
+			personagem.y = blocks[a].y - 171
 			onGround = true
 			break
 		else
